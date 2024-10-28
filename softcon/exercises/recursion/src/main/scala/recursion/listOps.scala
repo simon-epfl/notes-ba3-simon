11dcd17191128354a26c02b05904d6b14958471e
Androz 2091 <androz2091@gmail.com>
2024-10-28 08:56:18 +0100

    add min/max

--- softcon/exercises/recursion/src/main/scala/recursion/listOps.scala
+++ softcon/exercises/recursion/src/main/scala/recursion/listOps.scala
@@ -3,40 +3,64 @@ package recursion
 def length(l: IntList): Int =
   l match 
     case IntCons(head, tail) => 1 + length(tail)
-    case IntNil => 0
+    case IntNil() => 0
 
 def allPositiveOrZero(l: IntList): Boolean =
-  ???
+  l match
+    case IntCons(head, tail) => head >= 0 && allPositiveOrZero(tail)
+    case IntNil() => true
 
 def countPositive(l: IntList): Int =
-  ???
+  l match
+    case IntCons(head, tail) if head > 0 => 1 + countPositive(l.tail)
+    case IntCons(head, tail) if head <= 0 => countPositive(l.tail)
+    case IntNil() => 0
 
 def sum(l: IntList): Int =
-  ???
+  l match
+    case IntCons(head, tail) => head + sum(l.tail)
+    case IntNil() => 0
 
 def product(l: IntList): Int =
-  ???
+  l match
+    case IntCons(head, tail) => head * product(l.tail)
+    case IntNil() => 1
 
 def anyOdd(l: IntList): Boolean =
-  ???
+  l match
+    case IntCons(head, tail) if head % 2 == 0 => anyOdd(tail)
+    case IntCons(head, tail) => true
+    case IntNil() => false
 
 def decrement(l: IntList): IntList =
-  ???
+  l match
+    case IntCons(head, tail) => IntCons(head - 1, decrement(tail))
+    case IntNil() => IntNil()
 
 def collectEven(l: IntList): IntList =
-  ???
-
+  l match
+    case IntCons(head, tail) if head % 2 == 0 => IntCons(head, collectEven(tail))
+    case IntCons(head, tail) => collectEven(tail)
+    case IntNil() => IntNil()
+  
 def min(l: IntList): Int =
-  ???
+  l match
+    case IntNil() => throw new Exception("can not compute min")
+    case IntCons(head, IntNil()) => head
+    case IntCons(head, tail) => if head > tail.head then min(tail) else min(IntCons(head, tail.tail))
 
 def increment(l: IntList): IntList =
-  ???
+  l match
+    case IntNil() => IntNil()
+    case IntCons(head, tail) => IntCons(head + 1, increment(tail))
 
 def subtract(l: IntList): Int =
   ???
 
 def removeOdd(l: IntList): IntList =
-  ???
+  l match
+    case IntNil() => IntNil()
+    case IntCons(head, tail) => if head % 2 == 0 then IntCons(head, removeOdd(tail)) else removeOdd(tail)
 
 def countEven(l: IntList): Int =
   ???
@@ -58,7 +82,7 @@ def multiplyOdd(l: IntList): Int =
   ???
 
 def horner(x: Int, l: IntList): Int =
-  ???
+  if l.isEmpty then 0 else l.head + x * horner(x, l.tail)
 
 def capAtZero(l: IntList): IntList =
   ???
@@ -67,10 +91,12 @@ def removeZeroes(l: IntList): IntList =
   ???
 
 def reverseAppend(l1: IntList, l2: IntList): IntList =
-  ???
+  (l1, l2) match
+    case (IntNil(), l) => l
+    case (IntCons(head, tail), l) => reverseAppend(l1.tail, IntCons(l1.head, l2))
 
 def reverse(l: IntList): IntList =
-  ???
+  reverseAppend(l, IntNil())
 
 def takeWhilePositive(l: IntList): IntList =
   ???
@@ -100,7 +126,16 @@ def difference(l: IntList, L: IntList): IntList =
   ???
 
 def minMax(l: IntList): (Int, Int) =
-  ???
+  def minMaxHelper (l: IntList, currentMin: Int, currentMax: Int): (Int, Int) =
+    l match
+      case IntCons(head, tail) =>
+        if head > currentMax then minMaxHelper(tail, currentMin, head)
+        else if head < currentMin then minMaxHelper(tail, head, currentMax)
+        else minMaxHelper(tail, currentMin, currentMax)
+      case IntNil() => (currentMin, currentMax)
+
+  if l.isEmpty then throw new Exception() else minMaxHelper(l, l.head, l.head)
+  
 
 val Add = -1
 val Multiply = -2
---
+++ softcon/midterm.typ
@@ -0,0 +1,3 @@
+- récursion sur des `IntList`, ne pas oublier `IntCons(head, tail) =>` et pas `head :: tail -=>` comme dans une liste normale.
+
+- on peut faire un `case IntCons(head, tail) if head > 0 =>` (ajouter un test supplémentaire dans le case).
