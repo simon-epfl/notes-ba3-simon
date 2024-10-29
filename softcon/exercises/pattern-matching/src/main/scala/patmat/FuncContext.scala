c9dc831b16668750d1e1ba3e5795adee0ba2f129
Androz 2091 <androz2091@gmail.com>
2024-10-29 09:44:15 +0100

    add func context

--- softcon/exercises/pattern-matching/src/main/scala/patmat/FuncContext.scala
+++ softcon/exercises/pattern-matching/src/main/scala/patmat/FuncContext.scala
@@ -7,14 +7,12 @@ object FuncContext:
   type Context =
     String => LookupResult
 
-  def empty: Context =
-    ???
+  def empty: Context = (x: String) => NotFound
 
   def cons(name: String, value: Int, rem: Context): Context =
-    ???
+    (x: String) => if name == x then Ok(value) else rem(x)
 
-  def lookup(ctx: Context, name: String): LookupResult =
-    ???
+  def lookup(ctx: Context, name: String): LookupResult = ctx(name)
 
   def erase(ctx: Context, name: String): Context =
-    ???
+    (x: String) => if name == x then NotFound else ctx(x)
