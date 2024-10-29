617603149bf472b9e5cb31bbd95ca8944d8286c8
Androz 2091 <androz2091@gmail.com>
2024-10-29 09:39:59 +0100

    add unary nat

--- softcon/exercises/pattern-matching/src/main/scala/patmat/UnaryNatOps.scala
+++ softcon/exercises/pattern-matching/src/main/scala/patmat/UnaryNatOps.scala
@@ -9,22 +9,35 @@ object UnaryNatOps:
       case _ => Succ(fromInt(n - 1))
 
   def toInt(n: UnaryNat): Int =
-    ???
+    n match
+      case Zero => 0
+      case Succ(pred) => 1 + toInt(pred)
 
   def add(n: UnaryNat, m: UnaryNat): UnaryNat =
-    ???
+    n match
+      case Zero => m
+      case Succ(pred) => add(pred, Succ(m))
 
   def multiply(n: UnaryNat, m: UnaryNat): UnaryNat =
     m match
-      case Zero => n
+      case Zero => Zero
       case Succ(m0) => add(n, multiply(n, m0))
 
   def minus(n: UnaryNat, m: UnaryNat): UnaryNat =
-    ???
+    (n, m) match
+      case (_, Zero) => n
+      case (Zero, _) => Zero
+      case (Succ(pred), Succ(pred2)) => minus(pred, pred2)  
 
   def isEven(n: UnaryNat): Boolean =
-    ???
+    n match
+      case Zero => true
+      case Succ(Zero) => false
+      case Succ(Succ(pred)) => isEven(pred)
 
   def isOdd(n: UnaryNat): Boolean =
-    ???
+    n match
+      case Zero => false
+      case Succ(Zero) => true
+      case Succ(Succ(pred)) => isOdd(pred)
 
