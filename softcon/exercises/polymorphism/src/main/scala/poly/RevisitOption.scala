553a9294677bbcbf5e8611eb144896efe7b5128b
Androz 2091 <androz2091@gmail.com>
2024-10-29 18:29:27 +0100

    update optio

--- softcon/exercises/polymorphism/src/main/scala/poly/RevisitOption.scala
+++ softcon/exercises/polymorphism/src/main/scala/poly/RevisitOption.scala
@@ -4,18 +4,19 @@ import scala.collection.immutable.List
 import cs214.TODO
 
 def findFirstEvenNumber(l: List[Int]): Option[Int] =
-  ???
+  l match
+    case head :: next => if head % 2 == 0 then Some(head) else findFirstEvenNumber(next)
+    case Nil => None
 
 def parseStringToInt(s: String): Option[Int] =
-  ???
+  s.toIntOption
 
 def findSquareRoot(n: Int): Option[Double] =
-  ???
+  if n >= 0 then Some(Math.sqrt(n)) else None
 
 def findSquareRootFromString(s: String): Option[Double] =
-  ???
+  parseStringToInt(s).flatMap(findSquareRoot)
 
 val numberStrings = List("1", "2", "star", "4")
 
-val numbers =
-  TODO
+val numbers = numberStrings.flatMap(parseStringToInt)
