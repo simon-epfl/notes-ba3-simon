822dd47d9836ed84af36825d3d731fa8d849d71b
Androz 2091 <androz2091@gmail.com>
2024-10-29 10:36:27 +0100

    update folds/reducers

--- softcon/exercises/polymorphism/src/main/scala/poly/MyListOps.scala
+++ softcon/exercises/polymorphism/src/main/scala/poly/MyListOps.scala
@@ -5,46 +5,71 @@ import scala.annotation.tailrec
 import cs214.TODO
 
 def map[A, B](l: MyList[A])(f: A => B): MyList[B] =
-  ???
+  l match
+    case Nil => Nil
+    case Cons(x, xs) => Cons(f(x), map(xs)(f))
 
 def filter[A](l: MyList[A])(p: A => Boolean): MyList[A] =
-  ???
+  l match
+    case Nil => Nil
+    case Cons(x, xs) => if p(x) then Cons(x, filter(xs)(p)) else filter(xs)(p)
 
 def foldRight[A, B](l: MyList[A])(f: (A, B) => B, base: B): B =
-  ???
+  l match
+    case Nil => base
+    case _ => f(l.head, foldRight(l.tail)(f, base))
 
 def reduceRight[A](l: MyList[A])(f: (A, A) => A): A =
-  ???
+  l match
+    case Nil => throw new IllegalArgumentException()
+    case Cons(x, Nil) => x
+    case Cons(x, xs) => f(x, reduceRight(xs)(f))
 
 def forall[A](l: MyList[A])(p: A => Boolean): Boolean =
-  ???
+  l match
+    case Nil => true
+    case Cons(x, xs) => p(x) && forall(xs)(p)
 
 def exists[A](l: MyList[A])(p: A => Boolean): Boolean =
-  ???
+  l match
+    case Nil => false
+    case Cons(x, xs) => p(x) || exists(xs)(p)
 
 def zip[A, B](l1: MyList[A], l2: MyList[B]): MyList[(A, B)] =
-  ???
+  (l1, l2) match
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case _ => Cons((l1.head, l2.head), zip(l1.tail, l2.tail))
 
 def zipWith[A, B, C](l1: MyList[A], l2: MyList[B])(op: (A, B) => C): MyList[C] =
-  ???
+  (l1, l2) match
+    case (_, Nil) => Nil
+    case (Nil, _) => Nil
+    case _ => Cons(op(l1.head, l2.head), zipWith(l1.tail, l2.tail)(op))
 
 def elementsAsStrings[A](l: MyList[A]): MyList[String] =
-  ???
+  map(l)(_.toString)
 
 def length[A](l: MyList[A]): Int =
-  ???
+  foldRight(l)((x, acc) => 1 + acc, 0)
 
 def takeWhilePositive(l: MyList[Int]): MyList[Int] =
-  ???
+  foldRight(l)((x, acc) => if x >= 0 then Cons(x, acc) else Nil, Nil)
 
 def last[A](l: MyList[A]): A =
-  ???
+  reduceRight(l)((a, b) => b)
 
-val capitalizeString: MyList[Char] => MyList[Char] =
-  TODO
+val capitalizeString: MyList[Char] => MyList[Char] = (l) => map(l)(_.toUpper)
 
 def wordCount(l: MyList[Char]): Int =
-  ???
+  def discardWord(l: MyList[Char]): MyList[Char] =
+    if l.isEmpty || l.head.isWhitespace then l
+    else discardWord(l.tail)
+
+  if l.isEmpty then 0
+  else if l.head.isWhitespace then wordCount(l.tail)
+  else 1 + wordCount(discardWord(l))
+
 
 def append[A](l1: MyList[A], l2: MyList[A]): MyList[A] = l1 match
   case Nil         => l2
--- softcon/midterm.typ
+++ softcon/midterm.typ
@@ -13,3 +13,41 @@ enum Context:
   case Cons(name: String, value: Int, rem: Context)
 ```
 Une clef associée à une valeur dans un contexte est appelée un "Binding".
+
+== Folds
+
+#image("posts/folds.png")
+
+Le fold right demande une base (qui sera un accumulateur), ici c'est 0.
+
+== Reducers
+
+#image("posts/reducers.png", width: 90%)
+
+== Scala docs
+
+=== Traits, Class, companion objects
+
+Un trait est similaire à une interface Java :
+```scala
+trait EtreVivant {
+  def respirer(): String
+}
+```
+Une classe peut implémenter un ou plusieurs traits :
+```scala
+class Humain(val nom: String) extends EtreVivant {
+  def respirer(): String = s"$nom est un humain qui respire."
+  
+  def marcher(): String = s"$nom est en train de marcher."
+}
+```
+Un companion object permet de définir l'équivalent des méthodes statiques en Java :
+```scala
+object Humain {
+  def apply(nom: String): Humain = new Humain(nom)
+
+  // l'équivalent d'une méthode statique en Java
+  def descriptionGenerale(): String = "Les humains sont des êtres vivants qui respirent et peuvent marcher."
+}
+```
