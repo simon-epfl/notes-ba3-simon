2b477dac502f36319b235c5a2e6e573a7a8cd290
Androz 2091 <androz2091@gmail.com>
2024-11-02 16:44:22 +0100

    update tabuate

--- softcon/exercises/parallelism/src/main/scala/parallelism/ParenthesesBalancing.scala
+++ softcon/exercises/parallelism/src/main/scala/parallelism/ParenthesesBalancing.scala
@@ -5,10 +5,15 @@ import scala.collection.parallel.mutable.ParArray
 
 object ParenthesesBalancing:
   def isBalancedRecursive(str: List[Char]): Boolean =
-    ???
+    def isBalancedRecursiveHelper(str: List[Char], count: Int): Boolean =
+      str match
+        case head :: next => isBalancedRecursiveHelper(next, if head == '(' then count + 1 else if head == ')' then count - 1 else count)
+        case Nil => count == 0
+
+    isBalancedRecursiveHelper(str, 0)
 
   def isBalancedFold(str: List[Char]): Boolean =
-    ???
+    str.foldLeft(0)((acc, el) => acc + (if el == '(' then 1 else if el == ')' then -1 else 0)) == 0
 
   def isBalancedParSimple(str: List[Char]): Boolean =
     val foldingFunction: (Int, Char) => Int = ??? // your folding function
@@ -18,7 +23,7 @@ object ParenthesesBalancing:
     (numOpen == 0)
 
   def isBalancedPar(str: List[Char]): Boolean =
-    val seqOp: (Any, Char) => Any = ???
-    val combOp: (Any, Any) => Any = ???
+    val seqOp: (Int, Char) => Int = (base, el) => base + (if el == '(' then 1 else if el == ')' then -1 else 0)
+    val combOp: (Int, Int) => Int = (x, y) => x + y
 
-    str.par.aggregate(???)(seqOp, combOp) == ???
+    str.par.aggregate(0)(seqOp, combOp) == 0
--- softcon/exercises/parallelism/src/main/scala/parallelism/Tabulate.scala
+++ softcon/exercises/parallelism/src/main/scala/parallelism/Tabulate.scala
@@ -6,16 +6,19 @@ import scala.collection.parallel.CollectionConverters.*
 
 extension (a: Array.type)
   def seqTabulate[A: ClassTag](n: Int)(f: Int => A): Array[A] =
-    ???
+    (0 to n).map(f).toArray
 
 extension (p: ParArray.type) {
   def parTabulate[A: ClassTag](n: Int)(f: Int => A): ParArray[A] =
-    ???
+    if n < 10 then Array.seqTabulate(n)(f).par
+    else
+      val List(left, right) = Vector(n/2, n/2).par.map(v => ParArray.parTabulate(v)(f)).toList
+      left ++ right
 }
 
 extension [A](seq: Array[A])
   def zipWith[B, C: ClassTag](f: (A, B) => C)(other: Array[B]): Array[C] =
-    ???
+    ParArray.parTabulate(math.min(seq.length, other.length))(i => f(seq(i), other(i))).toArray
 
 def vectorAdd(a: Array[Int], b: Array[Int]) =
   a.zipWith((l: Int, r: Int) => l + r)(b)
