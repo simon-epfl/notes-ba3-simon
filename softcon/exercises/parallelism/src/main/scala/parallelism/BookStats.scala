7925c56909d14d120ac70e3475a4401a89b659e7
Androz 2091 <androz2091@gmail.com>
2024-11-02 14:15:32 +0100

    add bookstats

--- softcon/exercises/parallelism/src/main/scala/parallelism/BookStats.scala
+++ softcon/exercises/parallelism/src/main/scala/parallelism/BookStats.scala
@@ -11,19 +11,20 @@ object BookStats:
     Vector(Vector("the", "quick", "brown", "fox", "is", "jumping", "over"), Vector("the", "lazy", "dog"))
 
   def length(b: Book): Int =
-    ???
+    b.map(chapter => chapter.length).reduce((acc, currEl) => acc + currEl)
 
   def maxChapterLength(b: Book): Int =
-    ???
+    b.map(chapter => chapter.length).max
 
   def countWord(b: Book, w: Word): Int =
-    ???
+    b.map(chapter => chapter.count(word => word == w)).reduce((acc, el) => acc + el)
 
   def containsWord(b: Book, w: Word): Boolean =
-    ???
+    b.map(chapter => chapter.exists(word => word == w)).contains(true)
 
   def longestWord(b: Book): Word =
-    ???
+    b.flatten.groupBy(_.length).maxByOption(_._1).get._2.head
 
   def mostCommonWord(b: Book): Word =
-    ???
+    // from 8s to 2s by adding .par!
+    b.par.flatten.groupBy(countWord(b, _)).toList.maxByOption(_._1).get._2.head
